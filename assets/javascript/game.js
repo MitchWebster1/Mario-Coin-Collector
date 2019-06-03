0b46dd4f9995accb802d83eb5213c1a68a12eaba
Mitch Webster <mitch83083@gmail.com>
2019-06-02 20:21:57 -0400

    added changing backgrounds/need to fix audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
   let gameFinished = false;
   let total = 0;
   let target = 0;
-  let feedback = "";
 
   function randomNumber(min, max) {
     let number = Math.floor(Math.random() * (max - min) + min);
@@ -22,6 +21,14 @@ $(document).ready(function() {
     total = 0;
     newGame();
     updateDisplay();
+    $("#gameWin").hide();
+    $("#gameOver").hide();
+    // $("#container").hide();
+  }
+
+  function winningNumber() {
+    target = randomNumber(19, 120);
+    $("#target").text(target);
   }
 
   function newGame() {
@@ -35,16 +42,14 @@ $(document).ready(function() {
     $("#btn4").val(randomNumber(1, 12));
     updateDisplay();
   }
-  function winningNumber() {
-    target = randomNumber(19, 120);
-    $("#target").text(target);
-  }
 
   $(".btn").click(function() {
     if (gameFinished) {
       return;
     } else {
+      const audio = new Audio("../images/coin.wav");
       total += parseInt($(this).val());
+      audio.play();
       checkWin();
       updateDisplay();
     }
@@ -58,16 +63,48 @@ $(document).ready(function() {
     resetGame();
   });
 
+  function lose() {
+    $("#gameOver")
+      .show(50)
+      .delay(4000)
+      .hide(50);
+  }
+
+  function gameOver() {
+    const audio = new Audio("../images/gameOver.wav");
+    gameFinished = true;
+    losses++;
+    audio.play();
+    $("#container")
+      .hide(50, lose())
+      .delay(4000)
+      .show(50);
+  }
+
+  function win() {
+    $("#gameWin")
+      .show(50)
+      .delay(4000)
+      .hide(50);
+  }
+
+  function gameWin() {
+    const audio = new Audio("../images/win.wav");
+    gameFinished = true;
+    wins++;
+    audio.play();
+    $("#container")
+      .hide(50, win())
+      .delay(4000)
+      .show(50);
+  }
+
   function checkWin() {
     if (total === target) {
-      gameFinished = true;
-      feedback = "Congratulations you won!";
-      wins++;
+      gameWin();
       return;
     } else if (total > target) {
-      gameFinished = true;
-      feedback = "Sorry you lost try again!";
-      losses++;
+      gameOver();
       return;
     }
   }
