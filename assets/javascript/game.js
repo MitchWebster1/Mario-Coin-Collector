a4fb2eca9f2e6f65b66d2a398a8da481227ea425
Mitch Webster <mitch83083@gmail.com>
2019-06-05 20:00:17 -0400

    cleaned up code / added comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,21 +5,27 @@ $(document).ready(function() {
   let total = 0;
   let target = 0;
 
+  // Random number generator that takes in your min and max number
+  // that you would like to generate between.
   function randomNumber(min, max) {
     let number = Math.floor(Math.random() * (max - min) + min);
     return number;
   }
 
+  // Picks the target number for each game.
   function winningNumber() {
     target = randomNumber(19, 120);
     $("#target").text(target);
   }
 
+  // Updates the display to keep all values current
   function updateDisplay() {
     $("#total").text(total);
     $("#wins").text(wins);
     $("#losses").text(losses);
   }
+
+  // Resets all values to 0
   function resetGame() {
     wins = 0;
     losses = 0;
@@ -28,13 +34,12 @@ $(document).ready(function() {
     updateDisplay();
     $("#gameWin").hide();
     $("#gameOver").hide();
-    // $("#container").hide();
   }
 
+  // Generates a new target number and values for each button resets score
   function newGame() {
     total = 0;
     gameFinished = false;
-    feedback = "";
     winningNumber();
     $("#btn1").val(randomNumber(1, 12));
     $("#btn2").val(randomNumber(1, 12));
@@ -43,19 +48,24 @@ $(document).ready(function() {
     updateDisplay();
   }
 
+  // Button click for newGame
   $("#newGame").click(function() {
     newGame();
   });
 
+  // Button click for resetGame
   $("#resetGame").click(function() {
     resetGame();
   });
 
+  // Plays sounds from the html element
   function playSound(id) {
     const sound = document.getElementById(id);
     sound.play();
   }
 
+  // Button click takes value of whichever button is clicked and adds to total.
+  // Is there a way to not use this here and not have to write a function for each button?
   $(".btn").click(function() {
     if (gameFinished) {
       return;
@@ -67,46 +77,31 @@ $(document).ready(function() {
     }
   });
 
-  function lose() {
-    $("#gameOver")
+  // Hides the main screen and displays win or lose screen.
+  function changeScreen(id) {
+    $("#container")
+      .hide(50)
+      .delay(4000)
+      .show(50);
+    $(id)
       .show(50)
       .delay(4000)
       .hide(50);
   }
 
-  function gameOver() {
-    gameFinished = true;
-    losses++;
-    playSound("loseAudio");
-    $("#container")
-      .hide(50, lose())
-      .delay(4000)
-      .show(50);
-  }
-
-  function win() {
-    $("#gameWin")
-      .show(50)
-      .delay(4000)
-      .hide(50);
-  }
-
-  function gameWin() {
-    gameFinished = true;
-    wins++;
-    playSound("winAudio");
-    $("#container")
-      .hide(50, win())
-      .delay(4000)
-      .show(50);
-  }
-
+  // Checks total vs score to determine if game has been won or lost or to keep playing.
   function checkWin() {
     if (total === target) {
-      gameWin();
+      gameFinished = true;
+      changeScreen("#gameWin");
+      playSound("winAudio");
+      wins++;
       return;
     } else if (total > target) {
-      gameOver();
+      gameFinished = true;
+      changeScreen("#gameOver");
+      playSound("loseAudio");
+      losses++;
       return;
     }
   }
