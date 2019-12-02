a9a03032a9bd7a5dec269f5d64de0dd0821b8435
Mitch Webster <mitch83083@gmail.com>
2019-12-02 16:47:24 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# unit-4-game
+# Mario Coin Collector
+
+[**https://mitchwebster1.github.io/Mario-Coin-Collector/**](https://mitchwebster1.github.io/Mario-Coin-Collector/)
+
+## What does this project do?
+
+This is a game where the object is to match the target number. The values of the boxes are hidden from you until you click. It must be an exact match in order to win.
+
+## Why is this project useful?
+
+This project is useful for learning about jQuery and practicing passing data between functions.
+
+## How can users get started with this project?
+
+Just click the link at the top to check it out, and follow the instructions on screen.
+
+## Where can users get help with your project?
+
+Please contact me if you need any assistance.
+
+## Who maintains and contributes to the project?
+
+I am the sole contributer.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,109 +1,109 @@
-$(document).ready(function() {
-  let wins = 0;
-  let losses = 0;
-  let gameFinished = false;
-  let total = 0;
-  let target = 0;
+$(document).ready(function () {
+  let wins = 0
+  let losses = 0
+  let gameFinished = false
+  let total = 0
+  let target = 0
 
   // Random number generator that takes in your min and max number
   // that you would like to generate between.
-  function randomNumber(min, max) {
-    let number = Math.floor(Math.random() * (max - min) + min);
-    return number;
+  function randomNumber (min, max) {
+    const number = Math.floor(Math.random() * (max - min) + min)
+    return number
   }
 
   // Picks the target number for each game.
-  function winningNumber() {
-    target = randomNumber(19, 120);
-    $("#target").text(target);
+  function winningNumber () {
+    target = randomNumber(19, 120)
+    $('#target').text(target)
   }
 
   // Updates the display to keep all values current
-  function updateDisplay() {
-    $("#total").text(total);
-    $("#wins").text(wins);
-    $("#losses").text(losses);
+  function updateDisplay () {
+    $('#total').text(total)
+    $('#wins').text(wins)
+    $('#losses').text(losses)
   }
 
   // Resets all values to 0
-  function resetGame() {
-    wins = 0;
-    losses = 0;
-    total = 0;
-    newGame();
-    updateDisplay();
-    $("#gameWin").hide();
-    $("#gameOver").hide();
+  function resetGame () {
+    wins = 0
+    losses = 0
+    total = 0
+    newGame()
+    updateDisplay()
+    $('#gameWin').hide()
+    $('#gameOver').hide()
   }
 
   // Generates a new target number and values for each button resets score
-  function newGame() {
-    total = 0;
-    gameFinished = false;
-    winningNumber();
-    $("#btn1").val(randomNumber(1, 12));
-    $("#btn2").val(randomNumber(1, 12));
-    $("#btn3").val(randomNumber(1, 12));
-    $("#btn4").val(randomNumber(1, 12));
-    updateDisplay();
+  function newGame () {
+    total = 0
+    gameFinished = false
+    winningNumber()
+    $('#btn1').val(randomNumber(1, 12))
+    $('#btn2').val(randomNumber(1, 12))
+    $('#btn3').val(randomNumber(1, 12))
+    $('#btn4').val(randomNumber(1, 12))
+    updateDisplay()
   }
 
   // Button click for newGame
-  $("#newGame").click(function() {
-    newGame();
-  });
+  $('#newGame').click(function () {
+    newGame()
+  })
 
   // Button click for resetGame
-  $("#resetGame").click(function() {
-    resetGame();
-  });
+  $('#resetGame').click(function () {
+    resetGame()
+  })
 
   // Plays sounds from the html element
-  function playSound(id) {
-    const sound = document.getElementById(id);
-    sound.play();
+  function playSound (id) {
+    const sound = document.getElementById(id)
+    sound.play()
   }
 
   // Button click takes value of whichever button is clicked and adds to total.
   // Is there a way to not use this here and not have to write a function for each button?
-  $(".btn").click(function() {
+  $('.btn').click(function() {
     if (gameFinished) {
-      return;
+      
     } else {
-      total += parseInt($(this).val());
-      playSound("btn");
-      checkWin();
-      updateDisplay();
+      total += parseInt($(this).val())
+      playSound('btn')
+      checkWin()
+      updateDisplay()
     }
-  });
+  })
 
   // Hides the main screen and displays win or lose screen.
-  function changeScreen(id) {
-    $("#container")
+  function changeScreen (id) {
+    $('#container')
       .hide(50)
       .delay(4000)
-      .show(50);
+      .show(50)
     $(id)
       .show(50)
       .delay(4000)
-      .hide(50);
+      .hide(50)
   }
 
   // Checks total vs score to determine if game has been won or lost or to keep playing.
   function checkWin() {
     if (total === target) {
-      gameFinished = true;
-      changeScreen("#gameWin");
-      playSound("winAudio");
-      wins++;
-      return;
+      gameFinished = true
+      changeScreen('#gameWin')
+      playSound('winAudio')
+      wins++
+      
     } else if (total > target) {
-      gameFinished = true;
-      changeScreen("#gameOver");
-      playSound("loseAudio");
-      losses++;
-      return;
+      gameFinished = true
+      changeScreen('#gameOver')
+      playSound('loseAudio')
+      losses++
+      return
     }
   }
-  resetGame();
-});
+  resetGame()
+})
